9cd38d47f108980714b94a6661827806872270df
robert-virtual <robertocastill63@gmail.com>
2022-09-16 11:43:37 -0600

    paginas y navegacion

--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import "package:http/http.dart" as http;
 import 'package:google_sign_in/google_sign_in.dart';
 
 class UserBinding implements Bindings {
@@ -12,7 +15,45 @@ class UserController extends GetxController {
   GoogleSignIn googleSignIn = GoogleSignIn(scopes: [
     "https://www.googleapis.com/auth/spreadsheets",
   ]);
+  final spreadsheetId = "1hlcv__-71at852uml7TOKA_AS90qlkOQvcHOk-yq1bQ";
+  final baseUrl = "https://sheets.googleapis.com/v4/spreadsheets/";
   GoogleSignInAccount? account;
+
+  @override
+  void onInit() {
+    super.onInit();
+    googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? _account) {
+      account = _account;
+      update();
+      if (account != null) {
+        handleGetSheets(_account!);
+      }
+    });
+    googleSignIn.signInSilently();
+  }
+
+  Future<void> handleGetSheets(GoogleSignInAccount account) async {
+    /* _contactText = "Loading contact info..."; */
+    /* update(); */
+    final http.Response res = await http.get(
+        Uri.parse('${baseUrl}${spreadsheetId}/values/{Leche!A:D}'),
+        headers: await account.authHeaders);
+    if (res.statusCode != 200) {
+      /* _contactText = "People api gave a ${res.statusCode} response."; */
+      /* update(); */
+      return;
+    }
+    final Map<String, dynamic> data =
+        json.decode(res.body) as Map<String, dynamic>;
+    /* final String? namedContact = _pickFirstNamedContact(data); */
+    /* if (namedContact != null) { */
+    /*   _contactText = "I see you know $namedContact !!"; */
+    /* } else { */
+    /*   _contactText = "no contacts to display"; */
+    /* } */
+    /* update(); */
+  }
+
   void setAccount(GoogleSignInAccount _account) {
     account = _account;
     update();
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -10,12 +10,61 @@ class Login extends GetView<UserController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Lacteos San Esteban")),
-      body: Center(
-          child: Column(
-        children: const [
-          Text("Bienvenido a la app de Lacteos San Esteban"),
-        ],
-      )),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Bienvenido a la app de Lacteos San Esteban",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            _buildBody()
+          ],
+        )),
+      ),
     );
   }
+
+  Widget _buildBody() {
+    final GoogleSignInAccount? user = controller.account;
+    if (user != null) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          ListTile(
+              leading: GoogleUserCircleAvatar(identity: user),
+              title: Text(user.displayName ?? ""),
+              subtitle: Text(user.email)),
+          const Text("Inicio de session exitoso"),
+          ElevatedButton(
+              onPressed: _handleSignout, child: const Text("Cerrar session")),
+          ElevatedButton(
+              onPressed: () => controller.handleGetSheets(user),
+              child: const Text("Refresh"))
+        ],
+      );
+    }
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        const Text("Usted no ha iniciado session"),
+        ElevatedButton(onPressed: _handleSignin, child: const Text("Signin"))
+      ],
+    );
+  }
+
+  Future<void> _handleSignin() async {
+    try {
+      await controller.googleSignIn.signIn();
+    } catch (e) {
+      Get.snackbar("Error", e.toString());
+    }
+  }
+
+  Future<void> _handleSignout() => controller.googleSignIn.disconnect();
 }
