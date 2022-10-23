c57f412af8f867f81e76bbc4dc62f56d6679a445
robert-virtual <robertocastill63@gmail.com>
2022-10-22 23:08:26 -0600

    update google signin button

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -24,45 +24,55 @@ class Login extends GetView<UserController> {
 
   Widget _buildBody(GoogleSignInAccount? user) {
     if (user != null) {
-      return ListView(
-        padding: const EdgeInsets.all(8.0),
-        children: [
-          Image.asset("assets/san_esteban.jpg"),
-          ElevatedButton(
-              onPressed: () => Get.toNamed("/home"),
-              child: const Text("Inicio")),
-          ListTile(
-              leading: GoogleUserCircleAvatar(identity: user),
-              title: Text(user.displayName ?? ""),
-              subtitle: Text(user.email)),
-          const Text("Inicio de sesión exitoso"),
-          ElevatedButton(
-              onPressed: _handleSignout, child: const Text("Cerrar sesión")),
-        ],
+      return SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/san_esteban.jpg"),
+            ElevatedButton(
+                onPressed: () => Get.toNamed("/home"),
+                child: const Text("Inicio")),
+            ListTile(
+                leading: GoogleUserCircleAvatar(identity: user),
+                title: Text(user.displayName ?? ""),
+                subtitle: Text(user.email)),
+            const Text("Inicio de sesión exitoso"),
+            ElevatedButton(
+              onPressed: _handleSignout,
+              child: const Text("Cerrar sesión"),
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
       );
     }
-    return ListView(
-      padding: const EdgeInsets.all(8.0),
-      children: [
-        Image.asset("assets/san_esteban.jpg"),
-        const SizedBox(height: 10),
-        const Text("Usted no ha iniciado sesión"),
-        const SizedBox(height: 10),
-        ElevatedButton(
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Image.asset("assets/san_esteban.jpg"),
+          const SizedBox(height: 10),
+          const Text("Usted no ha iniciado sesión"),
+          const SizedBox(height: 10),
+          ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.white),
+                padding: MaterialStateProperty.all(const EdgeInsets.all(15.0))),
             onPressed: _handleSignin,
             child: Row(
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text("Iniciar sesión con google"),
-                const SizedBox(width: 10),
                 Image.asset(
                   "assets/google.png",
                   height: 25.0,
-                )
+                ),
+                const SizedBox(width: 25),
+                const Text("Iniciar sesión con Google"),
               ],
-            ))
-      ],
+            ),
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
     );
   }
 
